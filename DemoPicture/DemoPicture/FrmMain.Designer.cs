a638ef6125c718f267f9ececfd09b0f4c738ecb0
unknown <you@example.com>
2024-04-30 09:18:51 +0800

    xx

---
+++ DemoPicture/DemoPicture/FrmMain.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace DemoPicture
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spMain = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbFilter = new System.Windows.Forms.ComboBox();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.flowPnl = new System.Windows.Forms.FlowLayoutPanel();
+            this.folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.spMain)).BeginInit();
+            this.spMain.Panel1.SuspendLayout();
+            this.spMain.Panel2.SuspendLayout();
+            this.spMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // spMain
+            // 
+            this.spMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spMain.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.spMain.IsSplitterFixed = true;
+            this.spMain.Location = new System.Drawing.Point(0, 0);
+            this.spMain.Name = "spMain";
+            this.spMain.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // spMain.Panel1
+            // 
+            this.spMain.Panel1.Controls.Add(this.cmbFilter);
+            this.spMain.Panel1.Controls.Add(this.label2);
+            this.spMain.Panel1.Controls.Add(this.btnChange);
+            this.spMain.Panel1.Controls.Add(this.btnOpen);
+            this.spMain.Panel1.Controls.Add(this.txtFile);
+            this.spMain.Panel1.Controls.Add(this.label1);
+            // 
+            // spMain.Panel2
+            // 
+            this.spMain.Panel2.AutoScroll = true;
+            this.spMain.Panel2.Controls.Add(this.flowPnl);
+            this.spMain.Size = new System.Drawing.Size(800, 450);
+            this.spMain.SplitterWidth = 2;
+            this.spMain.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件路径:";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(78, 10);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(353, 21);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(437, 8);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "浏览...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(722, 8);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 3;
+            this.btnChange.Text = "转换";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(519, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "目标格式:";
+            // 
+            // cmbFilter
+            // 
+            this.cmbFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFilter.FormattingEnabled = true;
+            this.cmbFilter.Items.AddRange(new object[] {
+            "JPG",
+            "PNG",
+            "BMP",
+            "GIF",
+            "ICO"});
+            this.cmbFilter.Location = new System.Drawing.Point(582, 10);
+            this.cmbFilter.Name = "cmbFilter";
+            this.cmbFilter.Size = new System.Drawing.Size(121, 20);
+            this.cmbFilter.TabIndex = 5;
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.Title = "请选择图片文件";
+            // 
+            // flowPnl
+            // 
+            this.flowPnl.AutoScroll = true;
+            this.flowPnl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowPnl.Location = new System.Drawing.Point(0, 0);
+            this.flowPnl.Name = "flowPnl";
+            this.flowPnl.Size = new System.Drawing.Size(800, 398);
+            this.flowPnl.TabIndex = 0;
+            // 
+            // folderDialog
+            // 
+            this.folderDialog.Description = "选择保存图片的路径";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.spMain);
+            this.Name = "FrmMain";
+            this.Text = "图片转换器";
+            this.spMain.Panel1.ResumeLayout(false);
+            this.spMain.Panel1.PerformLayout();
+            this.spMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spMain)).EndInit();
+            this.spMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer spMain;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+        private System.Windows.Forms.FlowLayoutPanel flowPnl;
+        private System.Windows.Forms.FolderBrowserDialog folderDialog;
+    }
+}
+
---
+++ DemoPicture/DemoPicture/FrmMain.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoPicture
+{
+    public partial class FrmMain : Form
+    {
+        private string fileFilter="JPG文件|*.jpg|PNG文件|*.png|BMP文件|*.bmp|GIF文件|*.gif|ICON文件|*.ico";
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 打开图片
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            this.fileDialog.Filter = fileFilter;
+            this.fileDialog.Multiselect = true;
+            this.fileDialog.CheckFileExists = true;
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] fileNames = this.fileDialog.FileNames;
+                foreach(string fileName in fileNames)
+                {
+                    Bitmap bmp = new Bitmap(fileName);
+                    //保存图片名称
+                    bmp.Tag = Path.GetFileNameWithoutExtension(fileName);
+                    PictureBox box = new PictureBox();
+                    box.Image = bmp;
+                    box.Width = 105;
+                    box.Height = 150;
+                    box.BorderStyle = BorderStyle.FixedSingle;
+                    box.Padding = new Padding(2);
+                    this.flowPnl.Controls.Add(box);
+                }
+                this.txtFile.Text = Path.GetDirectoryName(fileNames[0]);
+
+            }
+        }
+
+        /// <summary>
+        /// 转换图片按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            if (this.cmbFilter.SelectedItem == null)
+            {
+                MessageBox.Show("没有选择要转换的图片类型");
+                return;
+            }
+            
+            if (this.flowPnl.Controls.Count == 0)
+            {
+                MessageBox.Show("没有要转换的图片");
+                return;
+            }
+            
+            string filter = this.cmbFilter.SelectedItem.ToString();
+            string dir = string.Empty;
+            if (this.folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                dir = this.folderDialog.SelectedPath;
+            }
+            else
+            {
+                return;
+            }
+            foreach (PictureBox box in this.flowPnl.Controls)
+            {
+                convertImage(dir, filter, new Bitmap(box.Image) { Tag=box.Image.Tag});
+            }
+
+        }
+
+        /// <summary>
+        /// 转换图片
+        /// </summary>
+        private void convertImage(string dir, string filter,Bitmap bmp)
+        {
+            string filePath = Path.Combine(dir, string.Format("{0}.{1}", bmp.Tag.ToString(), filter.ToLower()));
+            switch (filter)
+            {
+                case "JPG":
+                    bmp.Save(filePath, ImageFormat.Jpeg);
+                    break;
+                case "PNG":
+                    bmp.Save(filePath, ImageFormat.Png);
+                    break;
+                case "GIF":
+                    bmp.Save(filePath, ImageFormat.Gif);
+                    break;
+                case "BMP":
+                    bmp.Save(filePath, ImageFormat.Bmp);
+                    break;
+                case "ICO":
+                    Stream stream = File.Create(filePath);
+                    Icon icon = Icon.FromHandle(bmp.GetHicon());
+                    icon.Save(stream);       //   save the icon
+                    stream.Close();
+                    break;
+            }
+        }
+    }
+}
